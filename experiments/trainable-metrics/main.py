3d9a5ef5a6320b90e923ea629dae7deacffcf2af
Daniil Larionov <rexhaif.io@gmail.com>
2023-03-24 15:09:38 +0100

    experiments with llama+gptscore

---
+++ experiments/trainable-metrics/main.py
@@ -0,0 +1,15 @@
+from rich.logging import RichHandler
+import logging
+
+import comet
+
+logging.basicConfig(
+    level="NOTSET",
+    format="%(message)s",
+    datefmt="[%X]",
+    handlers=[RichHandler(rich_tracebacks=True)],
+)
+
+logger = logging.getLogger(__name__)
+
+logger.info(str(comet))
